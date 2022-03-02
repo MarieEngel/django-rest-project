74e6116030f0df041ab3b8966a266db2658c8d3f
Marie Engel <marie.engel@deliveryhero.com>
2022-03-02 12:43:49 +0100

    path for indiv user

--- consumer/users/urls.py
+++ consumer/users/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import index
+from .views import index, detail
 
 
 urlpatterns = [
-    path("", index),
+    path("", index, name="index"),
+    path("<id>/", detail, name="detail")
 ]
