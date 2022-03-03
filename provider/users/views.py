32f2809ce1ea35db096c880dd4d497a4883f8a78
Marie Engel <marie.engel@deliveryhero.com>
2022-03-03 09:30:30 +0100

    added PostViewSet

--- provider/users/views.py
+++ provider/users/views.py
@@ -1,9 +1,13 @@
 # Source: https://www.django-rest-framework.org/tutorial/quickstart/
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
-from .serializers import UserSerializer, GroupSerializer
+from rest_framework.parsers import JSONParser
+from .serializers import UserSerializer, GroupSerializer, PostSerializer
+from .models import Post
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -24,3 +28,55 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows posts to be viewed or edited.
+    """    
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+# @csrf_exempt
+# def post_list(request):
+#     """
+#     List all posts, or create a new post.
+#     """
+#     if request.method == 'GET':
+#         posts = Post.objects.all()
+#         serializer = PostSerializer(posts, many=True)
+#         return JsonResponse(serializer.data, safe=False)
+
+#     elif request.method == 'POST':
+#         data = JSONParser().parse(request)
+#         serializer = PostSerializer(data=data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data, status=201)
+#         return JsonResponse(serializer.errors, status=400)  
+
+# @csrf_exempt
+# def post_detail(request, pk):
+#     """
+#     Retrieve, update or delete a post.
+#     """
+#     try:
+#         post = Post.objects.get(pk=pk)
+#     except Post.DoesNotExist:
+#         return HttpResponse(status=404)
+
+#     if request.method == 'GET':
+#         serializer = PostSerializer(post)
+#         return JsonResponse(serializer.data)
+
+#     elif request.method == 'PUT':
+#         data = JSONParser().parse(request)
+#         serializer = PostSerializer(post, data=data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data)
+#         return JsonResponse(serializer.errors, status=400)
+
+#     elif request.method == 'DELETE':
+#         post.delete()
+#         return HttpResponse(status=204)
