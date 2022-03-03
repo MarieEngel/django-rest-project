cb2dc2de36e47d365ae3df0f2b2aad6e2cc7cdc4
Marie Engel <marie.engel@deliveryhero.com>
2022-03-03 11:53:02 +0100

    deleted unneccessary imports

--- provider/users/models.py
+++ provider/users/models.py
@@ -1,14 +1,10 @@
-from pyexpat import model
-from django.contrib.auth.models import User
 from django.db import models
-
 
 
 class Post(models.Model):
     title= models.CharField(max_length=100)
     body = models.TextField()
-    # timestamp = models.DateTimeField(auto_now_add=True)
-    # autor = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
     def __str__(self):
         return self.title
