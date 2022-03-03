7898f6e508422b7b530e8732f0337d82157bbd2b
Marie Engel <marie.engel@deliveryhero.com>
2022-03-03 21:16:38 +0100

    correction: id instead of pk

--- provider/users/views.py
+++ provider/users/views.py
@@ -52,26 +52,26 @@ class PostDetail(APIView):
     Retrieve, update or delete a snippet instance.
     """
 
-    def get_object(self, pk):
+    def get_object(self, id):
         try:
-            return Post.objects.get(pk=pk)
+            return Post.objects.get(pk=id)
         except Post.DoesNotExist:
             raise Http404
 
-    def get(self, request, pk, format=None):
-        post = self.get_object(pk)
+    def get(self, request, id, format=None):
+        post = self.get_object(id)
         serializer = PostSerializer(post)
         return Response(serializer.data)
 
-    def put(self, request, pk, format=None):
-        post = self.get_object(pk)
+    def put(self, request, id, format=None):
+        post = self.get_object(id)
         serializer = PostSerializer(post, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk, format=None):
-        post = self.get_object(pk)
+    def delete(self, request, id, format=None):
+        post = self.get_object(id)
         post.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
