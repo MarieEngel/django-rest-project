d8157c8dcc7f24188a628bf83e92c3285d4c2aa5
Marie Engel <marie.engel@deliveryhero.com>
2022-03-03 21:18:43 +0100

    added delete & edit

--- consumer/users/urls.py
+++ consumer/users/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import index, detail
+from .views import index, detail, delete_post, edit_post
 
 
-urlpatterns = [path("", index, name="index"), path("<id>/", detail, name="detail")]
+urlpatterns = [
+    path("", index, name="index"), 
+    path("<id>/", detail, name="detail"),
+    path("<id>/delete/", delete_post, name="delete_post"),
+    path("<id>/edit/", edit_post, name="edit_post"),
+    ]
