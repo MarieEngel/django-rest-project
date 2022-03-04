2163fb6bd85dbb53cf09fead133259f24c0203a1
Marie Engel <marie.engel@deliveryhero.com>
2022-03-04 10:37:08 +0100

    added path to add posts

--- consumer/users/urls.py
+++ consumer/users/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import index, detail, delete_post, edit_post
+from .views import index, detail, delete_post, edit_post, add_post
 
 
 urlpatterns = [
     path("", index, name="index"), 
+    path("add/", add_post, name="add_post"),
     path("<id>/", detail, name="detail"),
     path("<id>/delete/", delete_post, name="delete_post"),
     path("<id>/edit/", edit_post, name="edit_post"),
+   
     ]
