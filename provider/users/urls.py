84e0be2a6718b6358f18e7815617d4a94ee26a44
Marie Engel <marie.engel@deliveryhero.com>
2022-03-07 14:32:55 +0100

    deleted comments

--- provider/users/urls.py
+++ provider/users/urls.py
@@ -11,17 +11,13 @@ router.register(r"users", views.UserViewSet)
 router.register(r"groups", views.GroupViewSet)
 
 router.register(r"posts", views.PostViewSet)
-# router.register(r"post",views.post_list)
-# router.register(r"post",views.post_detail)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path("", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
-    # path("posts/", views.PostList.as_view()),
-    # path("posts/<int:id>/", views.PostDetail.as_view()),
+ 
 
 ]
 
-# urlpatterns = format_suffix_patterns(urlpatterns)
