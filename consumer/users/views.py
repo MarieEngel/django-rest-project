7beb1eb1e792642136be090c6a7ce7595c53c24b
Marie Engel <marie.engel@deliveryhero.com>
2022-03-07 10:20:48 +0100

    tokenbased authentication

--- consumer/users/views.py
+++ consumer/users/views.py
@@ -2,15 +2,16 @@ import httpx
 
 from django.shortcuts import redirect, render
 from .forms import PostForm
+from django.conf import settings
 
 
 async def index(request):
     async with httpx.AsyncClient() as client:
         response = await client.get(
-            "http://localhost:8000/users/users/", auth=("admin", "password")
+            "http://localhost:8000/users/users/", headers={"Authorization": f"Token {settings.AUTH_TOKEN}"},
         )
         posts_response = await client.get(
-            "http://localhost:8000/users/posts/", auth=("admin", "password")
+            "http://localhost:8000/users/posts/", headers={"Authorization": f"Token {settings.AUTH_TOKEN}"},
         )
     users = response.json()
     posts = posts_response.json()
@@ -22,7 +23,7 @@ async def detail(request, id):
     print("ID is ", id)
     async with httpx.AsyncClient() as client:
         response = await client.get(
-            f"http://localhost:8000/users/users/{id}/", auth=("admin", "password")
+            f"http://localhost:8000/users/users/{id}/", headers={"Authorization": f"Token {settings.AUTH_TOKEN}"},
         )
     user = response.json()
     print(user)
@@ -32,7 +33,7 @@ async def delete_post(request, id):
     if request.method == "POST":
         async with httpx.AsyncClient() as client:
             post_response = await client.delete(
-            f"http://localhost:8000/users/posts/{id}/", auth=("admin", "password")
+            f"http://localhost:8000/users/posts/{id}/", headers={"Authorization": f"Token {settings.AUTH_TOKEN}"},
             )
         return redirect("/users")
 
@@ -43,13 +44,13 @@ async def edit_post(request, id):
         async with httpx.AsyncClient() as client:
             post_response = await client.put(
                 f"http://localhost:8000/users/posts/{id}/",
-                auth=("admin", "password"),
+                headers={"Authorization": f"Token {settings.AUTH_TOKEN}"},
                 data=request.POST
             )
         return redirect("/users")
 
     async with httpx.AsyncClient() as client:
-        response = await client.get(f"http://localhost:8000/users/posts/{id}/", auth=("admin", "password"))
+        response = await client.get(f"http://localhost:8000/users/posts/{id}/", headers={"Authorization": f"Token {settings.AUTH_TOKEN}"},)
     post = response.json()
     print(post)
 
@@ -64,7 +65,7 @@ async def add_post(request):
         async with httpx.AsyncClient() as client:
             post_response = await client.post(
                 f"http://localhost:8000/users/posts/",
-                auth=("admin", "password"),
+                headers={"Authorization": f"Token {settings.AUTH_TOKEN}"},
                 data=request.POST
             )
         return redirect("/users")
